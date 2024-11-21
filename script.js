2ab578d624f1947b10d67167c27e0e01f0ec0686
EriksRancans <EriksRancans@users.noreply.github.com>
2024-11-21 00:20:41 +0000

    deploy: 94124291e2ae45647a92afd54d86a2d309e4bb36

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const serverUrl = "http://62.66.213.231:14718";
 
-console.log(`Using server URL: ${serverUrl}`); // Debugging: Check the replaced server URLs
-
 function checkServerStatus() {
-  fetch(serverUrl, { method: "GET", mode: "no-cors" })
+  fetch(serverUrl, { method: "GET", mode: "no-cors" }) // Use "no-cors" to bypass CORS issues
     .then(() => {
+      // Any response, even if it's not readable, means the server is online
       document.getElementById("status").textContent = "Online";
       document.getElementById("status").className = "online";
     })
     .catch(() => {
+      // If fetch fails, treat it as offline
       document.getElementById("status").textContent = "Offline";
       document.getElementById("status").className = "offline";
     });
 }
 
-// Check server status initially and every 10 seconds
+// Check server status on page load and every 10 seconds
 checkServerStatus();
 setInterval(checkServerStatus, 10000);
